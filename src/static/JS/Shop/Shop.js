2520e4217043a4092f07b01e23ba0e1fa110d9dd
GitCoder052023 <K.alam93899@gmail.com>
2024-09-22 19:45:55 +0530

    Successfully fixed tailwind css bug

--- src/static/JS/Shop/Shop.js
+++ src/static/JS/Shop/Shop.js
@@ -1,5 +1,5 @@
 const menCard = document.getElementById("Men_product-card")
 
 menCard.addEventListener("click", ()=>{
-    window.location.href = "/shop/Collection/Mens"
+    window.location.href = "/shop/Collection/Categories/Mens"
 })
