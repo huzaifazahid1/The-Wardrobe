d66c04d8f933fc6bcade515ef39c173073800a53
IbrahimAhmad77977 <ibrahimahmad77977@gmail.com>
2024-10-11 15:17:47 +0500

    collection page responsiveness

--- src/static/JS/Shop/Collection/Collection.js
+++ src/static/JS/Shop/Collection/Collection.js
@@ -76,7 +76,7 @@ const products = [
 
 const categoryButtons = document.querySelectorAll(".grid.grid-cols-4 button");
 const productContainer = document.querySelector(
-  ".grid.grid-cols-1.md\\:grid-cols-2.lg\\:grid-cols-4"
+  ".grid.grid-cols-2.md\\:grid-cols-2.lg\\:grid-cols-4"
 );
 
 const CATEGORIES = ["all", "women", "men", "accessories"];
